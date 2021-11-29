e1b1d66783d9b4f21013a12a5db9ed10396a1330
NattyScratty <67875231+NattyScratty@users.noreply.github.com>
2021-11-28 17:24:25 -0800

    Added pokemonList and another users for rule
    I am looking at the for rule of another user to get acclimated to how they work

--- pokemon-js-app/scripts.js
+++ pokemon-js-app/scripts.js
@@ -1,5 +1,11 @@
-alert('Hello World');
-
-// Favorite Food variable example
-let favoriteFood = "sunflower butter";
-document.write(favoriteFood);
+let pokemonList = [ 
+    {name: 'Charizard', height: 1.7, types: ["fire", "flying"]},
+    {name: 'Meowth', height: .4, types: ["normal","ghost"]},
+    {name: 'Mewtwo', height: 2, types: ["flying", "psychic"]},
+]
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write('<br>' + pokemonList[i].name + (' , height: ') + pokemonList[i].height);
+    if (pokemonList[i].height >= 2) {
+      document.write(' - Wow, that\'s big!');
+    }
+  }
