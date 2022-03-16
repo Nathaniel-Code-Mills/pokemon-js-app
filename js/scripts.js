75a1ae4d9b20ddac04836938a710ae6839b71d87
NattyScratty <67875231+NattyScratty@users.noreply.github.com>
2022-03-15 18:28:04 -0700

    An additional forEach that reprints the pokemonRepository after adding a pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -30,5 +30,14 @@ console.log(pokemonRepository.getAll());
 pokemonRepository.add({ name: 'Snorlax', height: .4, types: ["weird","tunneling"]  });
 console.log(pokemonRepository.getAll());
 
+
+//forEach function looping through the repository again now with a new pokemon added
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height < 2 && pokemon.height > .5) 
+  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This here Pokemon is a medium fellow!');
+  if (pokemon.height <= .4) 
+  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This here Pokemon is a small fellow!');
+  if (pokemon.height >= 2) 
   document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This Pokemon is massive!');
+});
 
