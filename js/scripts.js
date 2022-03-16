a8c5b1765e8df7990cf7819bb835dcd726954a37
NattyScratty <67875231+NattyScratty@users.noreply.github.com>
2022-03-15 18:24:40 -0700

    New IIFE Repository created
    This contains the original pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,19 @@
+//IIFE Repository
+let pokemonRepository = (function () {
+  let pokemonList = [ 
+    {name: 'Charizard', height: 1.7, types: ["fire", "flying"]},  
     {name: 'Meowth', height: .4, types: ["normal","ghost"]},
     {name: 'Mewtwo', height: 2, types: ["flying", "psychic"]},
+    ];
+  return {
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
     }
+  };
+})();
 
 
 
