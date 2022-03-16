23e0366388388077ea479737aa2a2dac3dd067c6
NattyScratty <67875231+NattyScratty@users.noreply.github.com>
2022-03-15 18:26:20 -0700

    added forEach Function that loops through repository
    This forEach loops through the pokemonList and writes the function if it the pokemon meets a certain criteria

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,15 @@ let pokemonRepository = (function () {
   };
 })();
 
+//forEach function looping through the repository
+pokemonRepository.getAll().forEach(function(pokemon) {
+  if (pokemon.height < 2 && pokemon.height > .5) 
+  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This here Pokemon is a medium fellow!');
+  if (pokemon.height <= .4) 
+  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This here Pokemon is a small fellow!');
+  if (pokemon.height >= 2) 
+  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This Pokemon is massive!');
+});
 
 
   document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This Pokemon is massive!');
