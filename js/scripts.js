2f8b8159b37ac53afdd4fdfa85ef36f5b230a60a
NattyScratty <67875231+NattyScratty@users.noreply.github.com>
2022-03-28 15:13:25 -0700

    Created a new function that creates buttons in the new UL class of "pokemon-list"
    This button logs when users click on each of the different pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,30 @@ let pokemonRepository = (function () {
     {name: 'Meowth', height: .4, types: ["normal","ghost"]},
     {name: 'Mewtwo', height: 2, types: ["flying", "psychic"]},
     ];
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+  
+  function getAll() {
+    return pokemonList;
+  }  
+  function addListItem(pokemon){
+    let pokemonList = document.querySelector(".pokemon-list");
+    let listPokemon = document.createElement("li");
+    let button = document.createElement("button");
+    button.innerText = pokemon.name;
+    button.classList.add("button-class"); 
+    button.addEventListener('click', function() {
+      showDetails(pokemon);
+    })
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+  }
+
+  function showDetails(pokemon){
+    console.log(pokemon);
+  }
+
   return {
     add: add,
     getAll: getAll,
