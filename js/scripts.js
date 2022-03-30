3a02ca299f2356620abf5bd922fc0c32f01f8e89
NattyScratty <67875231+NattyScratty@users.noreply.github.com>
2022-03-30 12:17:48 -0700

    Deleted Old Code

--- js/scripts.js
+++ js/scripts.js
@@ -38,34 +38,7 @@ let pokemonRepository = (function () {
 })();
 //End of IIFE
 
-//forEach function looping through the repository
-pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height < 2 && pokemon.height > .5) 
-  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This here Pokemon is a medium fellow!');
-  if (pokemon.height <= .4) 
-  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This here Pokemon is a small fellow!');
-  if (pokemon.height >= 2) 
-  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This Pokemon is massive!');
-});
 
-//Adding a new pokemone with differenet keys and properties
-console.log(pokemonRepository.getAll());
-pokemonRepository.add({ name: 'Snorlax', height: .4, types: ["weird","tunneling"]  });
-console.log(pokemonRepository.getAll());
-
-
-//forEach function looping through the repository again now with a new pokemon added
-pokemonRepository.getAll().forEach(function(pokemon) {
-  if (pokemon.height < 2 && pokemon.height > .5) 
-  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This here Pokemon is a medium fellow!');
-  if (pokemon.height <= .4) 
-  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This here Pokemon is a small fellow!');
-  if (pokemon.height >= 2) 
-  document.write('<br>' + pokemon.name + ' (height: '+ pokemon.height + ')' + ' - This Pokemon is massive!');
-});
-
-
-// Calling the function which creates the buttons for the pokemons
 pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.addListItem(pokemon);
 });
