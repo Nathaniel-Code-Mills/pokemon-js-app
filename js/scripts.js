f47c44c86b5e40cdd255ab7ec468a9e7039636d4
NattyScratty <67875231+NattyScratty@users.noreply.github.com>
2022-04-12 20:32:28 -0700

    Deleted Old Array

--- js/scripts.js
+++ js/scripts.js
@@ -125,11 +125,6 @@
 
 
 let pokemonRepository = (function () {
-  let pokemonList = [ 
-    {name: 'Charizard', height: 1.7, types: ["fire", "flying"]},  
-    {name: 'Meowth', height: .4, types: ["normal","ghost"]},
-    {name: 'Mewtwo', height: 2, types: ["flying", "psychic"]},
-    ];
   function add(pokemon) {
     pokemonList.push(pokemon);
   }
